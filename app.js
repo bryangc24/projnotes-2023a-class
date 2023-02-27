0946ed7ffce2c95b208cfaa5d1a76bba449b395d
bryangc24 <102575364+bryangc24@users.noreply.github.com>
2023-02-27 14:54:09 +0000

    ✨Middlewares

--- app.js
+++ app.js
@@ -18,11 +18,25 @@ var app = express();
 //we are declaring the localization of the views
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
+//que es app es una instancia de express
+//Register midleware
+// app.use ((req,res,next) => {
+// console.log("✌ se ha recivido una peticion");
+// next();
+// });
+// app.use((req,res,next)=>{
+//   console.log(`🙌 IP: ${req.ip}`);
+//   next();
+// }); 
+//Log all received requests
 app.use(logger('dev'));
+// Parse request data into json
 app.use(express.json());
+// Decode url info
 app.use(express.urlencoded({ extended: false }));
+//Parse client cookies into json
 app.use(cookieParser());
+// Set up the static file server
 app.use(express.static(path.join(__dirname, 'public')));
 //registering routes
 app.use('/', indexRouter);
