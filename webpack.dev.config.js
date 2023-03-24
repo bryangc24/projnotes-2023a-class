3f675f139260ff6af137f8c61a7bfac49877c67c
bryangc24 <102575364+bryangc24@users.noreply.github.com>
2023-03-24 15:08:22 +0000

    😎Acabando el codigo

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -4,6 +4,8 @@
 
 // Importing an file routing manager
 const path = require('path');
+// Importing Extract Plugin
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // We export a configuration object
 // that will be used by webpack
@@ -29,9 +31,9 @@ module.exports = {
     // 3.1 Static files folder
     static: path.join(__dirname, "public"),
     // 3.2 Development server port
-    port: 8080,
+    port: 3000,
     // 3.3 Defining the host
-    host: "localhost"
+    host: "0.0.0.0"
   },
    // Adding a module to webpack
    module: {
@@ -57,8 +59,16 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
     ]
-  }
+  },
+  plugins: [new MiniCssExtractPlugin({
+    // Archivo css de salida
+    filename: 'styles/app.css'
+  })]
 }
-
+    
