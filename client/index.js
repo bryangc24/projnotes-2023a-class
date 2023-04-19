41b9011d1759207457ff10a981ed4630362e6b30
bryangc24 <102575364+bryangc24@users.noreply.github.com>
2023-04-19 14:23:46 +0000

    ⭐EXELENTE

--- client/index.js
+++ client/index.js
@@ -1,3 +1,4 @@
 // cargando los estilos
-import './styles/style.css'
-console.log("🎉 Estilos cargados correctamente 🎉");
+import './styles/style.css';
+
+console.log('🎉 Estilos cargados correctamente 🎉');
