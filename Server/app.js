47bfb81960eac06e577f9324d5b3485332675868
bryangc24 <102575364+bryangc24@users.noreply.github.com>
2023-04-26 14:11:12 +0000

    😎Corregido app.js

--- Server/app.js
+++ Server/app.js
@@ -1,61 +1,64 @@
-import createError from 'http-errors'
+import createError from 'http-errors';
 // import the Express Library
 import express from 'express';
 // Is a Core-Node library to manage system paths
-import path from 'path'
+import path from 'path';
 // helps to parse client cookies
 import cookieParser from 'cookie-parser';
-//library to log http commuication
-import morgan from 'morgan'
-//importing routes
+// library to log http commuication
+import morgan from 'morgan';
+// importing routes
 import indexRouter from '@server/routes/index';
 import usersRouter from '@server/routes/users';
 import apiRouter from '@server/routes/api';
 
-//setting webpack modules
+// setting webpack modules
 import webpack from 'webpack';
 import WebpackDevMiddleware from 'webpack-dev-middleware';
 
-//importing webpack configuration
-import webpackConfig from '../webpack.dev.config';
+// importing webpack configuration
 import WebpackHotMiddleware from 'webpack-hot-middleware';
+import webpackConfig from '../webpack.dev.config';
 
 // we are creating the express instance
 const app = express();
 
-//get the execution node
-const nodeEnviroment = process.env.NODE_ENV || 'production'
+// get the execution node
+const nodeEnviroment = process.env.NODE_ENV || 'production';
 
-//deciding if we add webpack middleware or not
-if(nodeEnviroment === 'development'){
-  //Start webpack dev server
-  console.log("🏆Ejecutando modo desarrollo");
+// deciding if we add webpack middleware or not
+if (nodeEnviroment === 'development') {
+  // Start webpack dev server
+  console.log('🏆Ejecutando modo desarrollo');
   // Adding the key "mode" with its value develoment
-  webpackConfig.mode =nodeEnviroment;
+  webpackConfig.mode = nodeEnviroment;
   // Setting the port
-  webpackConfig.devServer.port = process.env.port
-  //setting up the HMR (Hot module replacement)
-  webpackConfig.entry = ["webpack-hot-middleware/client?reload=true$timeout=1000",
-  webpackConfig.entry
-];
-//creating the bundler
-const bundle = webpack(webpackConfig);
-// Enabling the webpack middleware
-app.use(WebpackDevMiddleware(bundle, {
-  publicPath: webpackConfig.output.publicPath
-}));
-// enabling the webpack hot HMR
-app.use(WebpackHotMiddleware(bundle));
-}else{
-  console.log("🧧 Ejecutando en modo produccion🧧");
+  webpackConfig.devServer.port = process.env.port;
+  // setting up the HMR (Hot module replacement)
+  webpackConfig.entry = [
+    'webpack-hot-middleware/client?reload=true$timeout=1000',
+    webpackConfig.entry,
+  ];
+  // creating the bundler
+  const bundle = webpack(webpackConfig);
+  // Enabling the webpack middleware
+  app.use(
+    WebpackDevMiddleware(bundle, {
+      publicPath: webpackConfig.output.publicPath,
+    }),
+  );
+  // enabling the webpack hot HMR
+  app.use(WebpackHotMiddleware(bundle));
+} else {
+  console.log('🧧 Ejecutando en modo produccion🧧');
 }
 
 // view engine setup
-//we are declaring the localization of the views
+// we are declaring the localization of the views
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-//que es app es una instancia de express
-//Register midleware
+// que es app es una instancia de express
+// Register midleware
 // app.use ((req,res,next) => {
 // console.log("✌ se ha recivido una peticion");
 // next();
@@ -63,29 +66,29 @@ app.set('view engine', 'hbs');
 // app.use((req,res,next)=>{
 //   console.log(`🙌 IP: ${req.ip}`);
 //   next();
-// }); 
-//Log all received requests
+// });
+// Log all received requests
 app.use(morgan('dev'));
 // Parse request data into json
 app.use(express.json());
 // Decode url info
 app.use(express.urlencoded({ extended: false }));
-//Parse client cookies into json
+// Parse client cookies into json
 app.use(cookieParser());
 // Set up the static file server
 app.use(express.static(path.join(__dirname, '../public')));
-//registering routes
+// registering routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api',apiRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) =>{
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
