b7335f5a366839e9b242c4b8e5e7422c0646dad2
bryangc24 <102575364+bryangc24@users.noreply.github.com>
2023-05-29 16:56:16 +0000

    🐱‍👤Conexion de BD MONGODB

--- server/config/configKeys.js
+++ server/config/configKeys.js
@@ -1,5 +1,6 @@
 // Importando el paquete Dotenv
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+
 // Con esta función se cargan las variables
 // de entorno, un aspecto importante es que en
 // caso de no existir el archivo ".env" esta
@@ -9,7 +10,9 @@ dotenv.config();
 // Crearemos un objeto que contendra
 // las llaves de configuración
 export default {
+  appVersion: process.env.APP_VERSION,
   homeUrl: `${process.env.APP_URL}:${process.env.PORT}`,
   port: process.env.PORT || '3000',
   ip: process.env.IP,
+  mongoUrl: process.env.DEV_DATABASE_URL,
 };
