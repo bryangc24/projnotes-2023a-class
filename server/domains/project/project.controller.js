38378697b1a5b2421156bbb8c41ef6471e673c06
bryangc24 <102575364+bryangc24@users.noreply.github.com>
2023-06-08 02:06:28 +0000

    🍀Borrando con Mongoose

--- server/domains/project/project.controller.js
+++ server/domains/project/project.controller.js
@@ -129,6 +129,19 @@ const editPut = async (req, res) => {
   }
 };
 
+// DELETE "/project/:id"
+const deleteProject = async (req, res) => {
+  // Extrayendo el id de los parametros
+  const { id } = req.params;
+  // Usando el modelo para borrar el proyecto
+  try {
+    const result = await ProjectModel.findByIdAndRemove(id);
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
 // Controlador user
 export default {
   // Action Methods
@@ -137,4 +150,5 @@ export default {
   addPost,
   edit,
   editPut,
+  deleteProject,
 };
